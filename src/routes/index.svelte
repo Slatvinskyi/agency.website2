a0b764f6228f13cd352b7f22e0b7ea6ab927d140
Slatvinskyi <andrii.slatvinskyi@gmail.com>
2021-08-31 19:56:55 +0300

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,29 @@
+<nav class="relative flex flex-wrap items-center justify-between px-2 py-3 bg-black mb-3">
+    <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
+      <div class="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
+        <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white" href="">
+          Black Color
+        </a>
+        
+      </div>
+      <div class="lg:flex flex-grow items-center " id="example-navbar-warning">
+        <ul class="flex  lg:flex-row list-none ml-auto">
+            <li class="nav-item">
+              <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="">
+                Discover
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="">
+                Profile
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="">
+                Setting
+              </a>
+            </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
