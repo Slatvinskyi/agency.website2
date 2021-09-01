76bbacae14dfc1451cfb7f53f7ad2eaeb0144af4
Slatvinskyi <andrii.slatvinskyi@gmail.com>
2021-09-01 23:20:06 +0300

    changed font and style of Navbar, added Team section

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -16,8 +16,7 @@
       <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
         <div class="pl-4 flex items-center">
           <a class="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#">
-            
-            Agency
+             Agency
           </a>
         </div>
         <div class="block lg:hidden pr-4">
@@ -31,24 +30,24 @@
         <div class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 bg-white lg:bg-transparent text-black p-4 lg:p-0 z-20" id="nav-content">
           <ul class="list-reset lg:flex justify-end flex-1 items-center">
             <li class="mr-3">
-              <a class="inline-block py-2 px-4 text-black font-bold no-underline" href="#">Home</a>
+              <a class="toggleColour text-black text-sm font-bold leading-relaxed  mr-4  whitespace-nowrap uppercase  inline-block py-2 px-4 text-black  no-underline" href="#">Home</a>
             </li>
             <li class="mr-3">
-              <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#">About</a>
+              <a class="toggleColour text-white text-sm font-bold leading-relaxed  mr-4  whitespace-nowrap uppercase  inline-block py-2 px-4 text-black  no-underline" href="#">About</a>
             </li>
             <li class="mr-3">
-              <a class="inline-block text-black no-underline hover:text-gray-800 hover:text-underline py-2 px-4" href="#">Blog</a>
+              <a class="toggleColour text-white text-sm font-bold leading-relaxed  mr-4  whitespace-nowrap uppercase  inline-block py-2 px-4 text-black  no-underline" href="#">Blog</a>
             </li>
           </ul>
           <button
             id="navAction"
-            class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+            class="mx-auto lg:mx-0 hover:no-underline bg-white text-gray-800 font-bold rounded-full mt-4 lg:mt-0 py-4 px-8 shadow opacity-75 focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out "
           >
             Action
           </button>
         </div>
       </div>
-      <hr class="border-b border-gray-100 opacity-25 my-0 py-0" />
+      
     </nav>
     <!--Hero-->
     <div class="pt-24">
@@ -92,6 +91,8 @@
         </g>
       </svg>
     </div>
+    <!--Team-->
+    
     <!--Title start-->
     <section class="bg-white border-b py-8">
       <div class="container max-w-5xl mx-auto m-8">
@@ -117,18 +118,14 @@
             </p>
           </div>
           <div class="w-full sm:w-1/2 p-6">
-            <svg class="w-full sm:h-64 mx-auto" viewBox="0 0 1177 598.5" xmlns="http://www.w3.org/2000/svg">
-              <title>travel booking</title>
-              <!--Place for picture-->
-            </svg>
+            <img class="rounded-xl" src="https://source.unsplash.com/random/605x350" />
+            
           </div>
         </div>
         <div class="flex flex-wrap flex-col-reverse sm:flex-row">
           <div class="w-full sm:w-1/2 p-6 mt-6">
-            <svg class="w-5/6 sm:h-64 mx-auto" viewBox="0 0 1176.60617 873.97852" xmlns="http://www.w3.org/2000/svg">
-              <title>connected world</title>
-              <!--Place for picture-->
-            </svg>
+            <img class="rounded-xl" src="https://source.unsplash.com/random/605x350" />
+            
           </div>
           <div class="w-full sm:w-1/2 p-6 mt-6">
             <div class="align-middle">
@@ -147,6 +144,148 @@
           </div>
         </div>
       </div>
+
+      <!--Team-->
+      <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+          <div>
+            <p class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+              Core Team
+            </p>
+          </div>
+          <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+            <span class="relative inline-block">
+              <svg viewBox="0 0 52 24" fill="currentColor" class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
+                <defs>
+                  <pattern id="1d4040f3-9f3e-4ac7-b117-7d4009658ced" x="0" y="0" width=".135" height=".30">
+                    <circle cx="1" cy="1" r=".7"></circle>
+                  </pattern>
+                </defs>
+                <rect fill="url(#1d4040f3-9f3e-4ac7-b117-7d4009658ced)" width="52" height="24"></rect>
+              </svg>
+              <span class="relative">Welcome</span>
+            </span>
+            our talented team of professionals
+          </h2>
+          <p class="text-base text-gray-700 md:text-lg">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque rem aperiam, eaque ipsa quae.
+          </p>
+        </div>
+        <div class="grid gap-10 sm:grid-cols-2 lg:grid-cols-4">
+          <div>
+            <div class="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+              <img class="object-cover w-full h-56 md:h-64 xl:h-80" src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260" alt="Person" />
+              <div class="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+                <p class="mb-1 text-lg font-bold text-gray-100">Oliver Aguilerra</p>
+                <p class="mb-4 text-xs text-gray-100">Product Manager</p>
+                <p class="mb-4 text-xs tracking-wide text-gray-400">
+                  Vincent Van Gogh’s most popular painting, The Starry Night.
+                </p>
+                <div class="flex items-center justify-center space-x-3">
+                  <a href="/" class="text-white transition-colors duration-300 hover:text-teal-accent-400">
+                    <svg viewBox="0 0 24 24" fill="currentColor" class="h-5">
+                      <path
+                        d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z"
+                      ></path>
+                    </svg>
+                  </a>
+                  <a href="/" class="text-white transition-colors duration-300 hover:text-teal-accent-400">
+                    <svg viewBox="0 0 24 24" fill="currentColor" class="h-5">
+                      <path
+                        d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z"
+                      ></path>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+              <img class="object-cover w-full h-56 md:h-64 xl:h-80" src="https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260" alt="Person" />
+              <div class="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+                <p class="mb-1 text-lg font-bold text-gray-100">Marta Clermont</p>
+                <p class="mb-4 text-xs text-gray-100">Design Team Lead</p>
+                <p class="mb-4 text-xs tracking-wide text-gray-400">
+                  Amet I love liquorice jujubes pudding croissant I love pudding.
+                </p>
+                <div class="flex items-center justify-center space-x-3">
+                  <a href="/" class="text-white transition-colors duration-300 hover:text-teal-accent-400">
+                    <svg viewBox="0 0 24 24" fill="currentColor" class="h-5">
+                      <path
+                        d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z"
+                      ></path>
+                    </svg>
+                  </a>
+                  <a href="/" class="text-white transition-colors duration-300 hover:text-teal-accent-400">
+                    <svg viewBox="0 0 24 24" fill="currentColor" class="h-5">
+                      <path
+                        d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z"
+                      ></path>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+              <img class="object-cover w-full h-56 md:h-64 xl:h-80" src="https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260" alt="Person" />
+              <div class="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+                <p class="mb-1 text-lg font-bold text-gray-100">Anthony Geek</p>
+                <p class="mb-4 text-xs text-gray-100">CTO, Lorem Inc.</p>
+                <p class="mb-4 text-xs tracking-wide text-gray-400">
+                  Apple pie macaroon toffee jujubes pie tart cookie caramels.
+                </p>
+                <div class="flex items-center justify-center space-x-3">
+                  <a href="/" class="text-white transition-colors duration-300 hover:text-teal-accent-400">
+                    <svg viewBox="0 0 24 24" fill="currentColor" class="h-5">
+                      <path
+                        d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z"
+                      ></path>
+                    </svg>
+                  </a>
+                  <a href="/" class="text-white transition-colors duration-300 hover:text-teal-accent-400">
+                    <svg viewBox="0 0 24 24" fill="currentColor" class="h-5">
+                      <path
+                        d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z"
+                      ></path>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
+              <img class="object-cover w-full h-56 md:h-64 xl:h-80" src="https://images.pexels.com/photos/3747435/pexels-photo-3747435.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260" alt="Person" />
+              <div class="absolute inset-0 flex flex-col justify-center px-5 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
+                <p class="mb-1 text-lg font-bold text-gray-100">Alice Melbourne</p>
+                <p class="mb-4 text-xs text-gray-100">Human Resources</p>
+                <p class="mb-4 text-xs tracking-wide text-gray-400">
+                  Lorizzle ipsum bling bling sit amizzle, consectetuer adipiscing elit.
+                </p>
+                <div class="flex items-center justify-center space-x-3">
+                  <a href="/" class="text-white transition-colors duration-300 hover:text-teal-accent-400">
+                    <svg viewBox="0 0 24 24" fill="currentColor" class="h-5">
+                      <path
+                        d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z"
+                      ></path>
+                    </svg>
+                  </a>
+                  <a href="/" class="text-white transition-colors duration-300 hover:text-teal-accent-400">
+                    <svg viewBox="0 0 24 24" fill="currentColor" class="h-5">
+                      <path
+                        d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z"
+                      ></path>
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            
     </section>
     <section class="bg-white border-b py-8">
       <div class="container mx-auto flex flex-wrap pt-4 pb-12">
@@ -224,6 +363,52 @@
         </div>
       </div>
     </section>
+    <!--Team-->
+    
+<!--Pictures-->
+    <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div class="grid gap-10 lg:grid-cols-2">
+        <div class="flex flex-col justify-center md:pr-8 xl:pr-0 lg:max-w-lg">
+          <div class="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-teal-accent-400">
+            <svg class="text-teal-900 w-7 h-7" viewBox="0 0 24 24">
+            <!--Icon here-->
+            </svg>
+          </div>
+          <div class="max-w-xl mb-6">
+            <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
+              Let us handle<br class="hidden md:block" />
+              your next
+              <span class="inline-block text-deep-purple-accent-400">destination</span>
+            </h2>
+            <p class="text-base text-white md:text-lg">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae. explicabo.
+            </p>
+          </div>
+          <div>
+            <a href="/" aria-label="" class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">
+              Learn more
+              <svg class="inline-block w-3 ml-2" fill="currentColor" viewBox="0 0 12 12">
+                <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z"></path>
+              </svg>
+            </a>
+          </div>
+        </div>
+        <div class="flex items-center justify-center -mx-4 lg:pl-8">
+          <div class="flex flex-col items-end px-3">
+            <img
+              class="object-cover mb-6 rounded shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56"
+              src="https://source.unsplash.com/random/605x350"
+              alt=""
+            />
+            <img class="object-cover w-20 h-20 rounded shadow-lg sm:h-32 xl:h-40 sm:w-32 xl:w-40" src="https://source.unsplash.com/random/350x350" alt="" />
+          </div>
+          <div class="px-3">
+            <img class="object-cover w-40 h-40 rounded shadow-lg sm:h-64 xl:h-80 sm:w-64 xl:w-80" src="https://source.unsplash.com/random/250x250" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+
     <!--Portfolio-->
     <section class="bg-gray-100 py-8">
       <div class="container mx-auto px-2 pt-4 pb-12 text-gray-800">
